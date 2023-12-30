31eb2f782d2d2217b0bfdbec15eff16cd0c36466
Nam Pham <allstarsmen@gmail.com>
2023-12-30 18:07:32 +0700

    Fixed: Nav return wrong navigation

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -1,9 +1,26 @@
 import { Routes } from "@angular/router";
 
 const routes: Routes = [
-  { path: '', title: 'Home', loadChildren: () => import('./pages/home/home.module').then(module => module.HomeModule) },
-  { path: 'categories', title: 'Categories', loadChildren: () => import('./pages/categories/categories.module').then(module => module.CategoriesModule) },
-  { path: 'category', title: 'New Category', loadChildren: () => import('./pages/category/category.module').then(module => module.CategoryModule) }
+  {
+    path: '',
+    title: 'Home',
+    loadChildren: () => import('./pages/home/home.module').then(module => module.HomeModule)
+  },
+  {
+    path: 'categories',
+    title: 'Categories',
+    loadChildren: () => import('./pages/categories/categories.module').then(module => module.CategoriesModule)
+  },
+  {
+    path: 'category',
+    title: 'New Category',
+    loadChildren: () => import('./pages/category/category.module').then(module => module.CategoryModule)
+  },
+  {
+    path: 'category/:id',
+    title: 'Edit Category',
+    loadChildren: () => import('./pages/category/category.module').then(module => module.CategoryModule)
+  }
 ];
 
 export default routes;
